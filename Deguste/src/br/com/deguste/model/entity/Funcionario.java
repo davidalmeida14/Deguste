27240fb8222a9b313989c7ed44de0e2b5fa49f3c
David Almeida <david_almeida_1@hotmail.com>
2016-03-10 17:53:26 -0300

    commit

--- Deguste/src/br/com/deguste/model/entity/Funcionario.java
+++ Deguste/src/br/com/deguste/model/entity/Funcionario.java
@@ -1,6 +1,25 @@
 package br.com.deguste.model.entity;
 
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+@ManagedBean
 public class Funcionario {
-	String teste = "Tese";
-	String teste6 = "LKjalsd";
+	private String nome;
+	private Date dataAdmissao;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getDataAdmissao() {
+		return dataAdmissao;
+	}
+	public void setDataAdmissao(Date dataAdmissao) {
+		this.dataAdmissao = dataAdmissao;
+	}
+	
+	
 }
